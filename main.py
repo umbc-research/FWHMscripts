279d1514647a0cef7cb92871954784eb6fb703a9
neutronplsr <c241@umbc.edu>
2023-10-16 11:26:50 -0400

    setting up env

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import astropy
 import scipy
 import numpy
+import astropy.io
 
 """
 Authors:Olivia Chiarini
@@ -8,11 +9,10 @@ Research Advisors: Dr. Meyer and Roy Prouty
 Purpose: To analyze FWHM data from .fits files given by UMBC's Obsertvatory, 
 compare these to SHARPCAP to see how they are getting their histograms 
 for FWHM and focusing analyizse. 
-
 """
 
 """
-STEPS:
+GOALS:
 Load in FITS file
 Find Sources
 Fit Profiles
@@ -23,7 +23,39 @@ Fit Profiles
 Calculate FWHM
 Convert from pixels at arcseconds
 """
-print("hello world")
 
+"""
+TO DOS:
+take userinput
+make folder for users to place fits files in
+make filepath dynamio
+figure out how to display data
+"""
+
+if __name__ == '__main__':
+  print("work in progress sorry :(")
+  
+
+
+
+#STEP ONE: load in fits file
+def loadFits():
+  fits_filename = fits.util.get_testdata_filepath('test0.fits')
+  hdul = fits.open(fits_filename)
+
+
+#STEP TWO: find sources
+
+
+#STEP THREE:fit profiles
+
+
+#STEP FOUR: extract STDVS
+
+
+#STEP FIVE: convert from pixels ot arcseconds
+
+
+#STEP SIX: display and export this info
 
 
