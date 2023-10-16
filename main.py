238c8b3410cf82832004c3faa5c345f96289ffad
Olivia Chiarini <148074758+neutronplsr@users.noreply.github.com>
2023-10-16 09:12:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import astropy
+import scipy
+import numpy
 
+"""
+Authors:Olivia Chiarini
+Research Advisors: Dr. Meyer and Roy Prouty
+Purpose: To analyze FWHM data from .fits files given by UMBC's Obsertvatory, 
+compare these to SHARPCAP to see how they are getting their histograms 
+for FWHM and focusing analyizse. 
+
+"""
+
+"""
+STEPS:
+Load in FITS file
+Find Sources
+Fit Profiles
+  -Vertical
+  -Horizontal
+  -Radial
+ Extract STDV
+Calculate FWHM
+Convert from pixels at arcseconds
+"""
