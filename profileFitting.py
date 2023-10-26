02801d85a32f5cf40d016e857486cc7a9a8d0bb0
Roy Prouty <proutyr1@umbc.edu>
2023-10-26 11:18:37 -0400

    Cleaned up profileFitting and left debugging plots in __main__ Cleaned up main Wrote code to identify subframes, extract profiles (radial, hori, verti) Wrote code to fit 1-D Gaussian to each profile Wrote code to report FWHM from each fit

--- profileFitting.py
+++ profileFitting.py
@@ -1,7 +1,5 @@
 from scipy.optimize import curve_fit
-from numpy import exp, linspace, random
-
-
+from numpy import exp, linspace, random, indices,bincount, sqrt
 
 #meaningless comment
 def gaussian_1d(x, mu, sigma, amplitude, offset):
@@ -15,43 +13,74 @@ def fit_gaussian_1d(x_data, y_data, bounds=([40, 1, 500, 1],[60,100,75000, 25000
     params, cov = curve_fit(gaussian_1d, x_data, y_data, bounds=([40, 1, 500, 1],[60,100,75000, 25000]))
     return params
 
+def extract_radial_data(data, xC, yC):
+    #Get matrix of integer indices associated with subFrame
+    y, x = indices((data.shape))
+
+    #Generate matrix of radius values
+    r = sqrt((x - xC)**2 + (y - yC)**2)
+
+    #Force integer values (np.sqrt gives floats)
+    r = r.astype(int)
+
+    #Generate a histogram of radius bin, each weighed by corresponding counts
+    weightedRadiusHistogram = bincount(r.ravel(), weights=data.ravel())
+    unweightedRadiusHistogram = bincount(r.ravel())
+
+    #Get average for each radius bin
+    averageCountsPerRadiusBin = weightedRadiusHistogram / unweightedRadiusHistogram
+    return averageCountsPerRadiusBin
+
+
+
 if __name__ == "__main__":
     from sys import argv
     from matplotlib import pyplot as plt
     
     mu, sigma, amplitude, offset = float(argv[1]), float(argv[2]), float(argv[3]), float(argv[4]) 
-    
-    #find which fit you want to calculate and act accordingly
-    #0 for vert, 1 for horz, 2 for 2d, 3 for radial, 4 for all and compare
-    match input:
-        case 0:
-            print("calculating vertical fit...")
-            
-        case 1:
-            print("calculating horizontal fit...")
-            
-        case 2:
-            print("calculating 2-d fit based on horizontal and vertical fits...")
-            
-        case 3:
-            print("calculating radial fit...")
-            
-        case 4:
-            print("calcualting all possible fits...")
-            
-        case _:
-            #as of rn, this comes up with random data and fits it
-            print("running default case...")    
+    x = linspace(0,100,1000)
+    y = gaussian_1d(x, mu, sigma, amplitude, offset)
 
-            x = linspace(0,100,1000)
-            y = gaussian_1d(x, mu, sigma, amplitude, offset)
+    y_rand = y + random.rand(len(y))*2.5 - 1.25
+    m, s, a, o = fit_gaussian_1d(x[::25], y_rand[::25])
+    plt.plot(x[::25], y_rand[::25], 'r.')
+    plt.plot(x[::25], gaussian_1d(x[::25], m, s, a, o), 'b')
+    print(m, s, a, o)
+    plt.grid(1)
+    plt.show()
+
+
+    # #find which fit you want to calculate and act accordingly
+    # #0 for vert, 1 for horz, 2 for 2d, 3 for radial, 4 for all and compare
+    # match input:
+    #     case 0:
+    #         print("calculating vertical fit...")
+            
+    #     case 1:
+    #         print("calculating horizontal fit...")
+            
+    #     case 2:
+    #         print("calculating 2-d fit based on horizontal and vertical fits...")
+            
+    #     case 3:
+    #         print("calculating radial fit...")
+            
+    #     case 4:
+    #         print("calcualting all possible fits...")
+            
+    #     case _:
+    #         #as of rn, this comes up with random data and fits it
+    #         print("running default case...")    
+
+    #         x = linspace(0,100,1000)
+    #         y = gaussian_1d(x, mu, sigma, amplitude, offset)
     
-            y_rand = y + random.rand(len(y))*2.5 - 1.25
-            m, s, a, o = fit_gaussian_1d(x[::25], y_rand[::25])
-            plt.plot(x[::25], y_rand[::25], 'r.')
-            plt.plot(x[::25], gaussian_1d(x[::25], m, s, a, o), 'b')
-            print(m, s, a, o)
-            plt.grid(1)
-            plt.show()
+    #         y_rand = y + random.rand(len(y))*2.5 - 1.25
+    #         m, s, a, o = fit_gaussian_1d(x[::25], y_rand[::25])
+    #         plt.plot(x[::25], y_rand[::25], 'r.')
+    #         plt.plot(x[::25], gaussian_1d(x[::25], m, s, a, o), 'b')
+    #         print(m, s, a, o)
+    #         plt.grid(1)
+    #         plt.show()
             
     
